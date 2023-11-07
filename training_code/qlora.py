e151bf4bbe6ccb7f9de1aaf4c6fbc2c0ad14f768
an1006634493 <35823777+an1006634493@users.noreply.github.com>
2023-11-07 18:53:58 +0800

    Update qlora.py

--- training_code/qlora.py
+++ training_code/qlora.py
@@ -1,5 +1,5 @@
 # This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# NOTICE.md file in the root directory of this source tree.
 
 from collections import defaultdict
 import copy
