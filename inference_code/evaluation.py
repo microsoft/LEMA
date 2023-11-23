7f8f7ae6b4dd8a9323184144e9417f04496a4ff3
an1006634493 <35823777+an1006634493@users.noreply.github.com>
2023-11-23 16:32:37 +0800

    Update evaluation.py

--- inference_code/evaluation.py
+++ inference_code/evaluation.py
@@ -6,7 +6,7 @@ import argparse
 import os
 import pdb
 
-from utils import get_final_result_gsm8k, get_final_result_math
+from utils import get_final_result_gsm8k, get_final_result_math, get_csqa_match
 
 
 if __name__ == "__main__":
@@ -27,30 +27,72 @@ if __name__ == "__main__":
     acc_list = []
     with open(file_label, 'r', encoding='utf-8') as f_label, \
             open(file_pred, 'r', encoding='utf-8') as f_pred:
-        labels = [json.loads(line)['completion'] for line in f_label.readlines()]
-        preds = [json.loads(line)['samples'][0] for line in f_pred.readlines()]
+        label_infos = [json.loads(line) for line in f_label.readlines()]
+        pred_infos = [json.loads(line) for line in f_pred.readlines()]
 
-        assert len(labels) == len(preds)
+        assert len(label_infos) == len(pred_infos)
 
-        for label, pred in zip(labels, preds):
+        for label_info, pred_info in zip(label_infos, pred_infos):
 
             if 'gsm8k' in args.test_data_file:
+                pred = pred_info['samples'][0]
+                label = label_info['completion']
+
                 pred_result = get_final_result_gsm8k(pred)
                 label_result = get_final_result_gsm8k(label)
+
+                assert label_result is not None
+                assert label_result != ''
+                assert label_result != 0
+
+                if pred_result == label_result:
+                    acc_list.append(1)
+                else:
+                    acc_list.append(0)
+
             elif 'math' in args.test_data_file:
+                pred = pred_info['samples'][0]
+                label = label_info['completion']
+
                 pred_result = get_final_result_math(pred)
                 label_result = get_final_result_math(label)
+
+                assert label_result is not None
+                assert label_result != ''
+                assert label_result != 0
+
+                if pred_result == label_result:
+                    acc_list.append(1)
+                else:
+                    acc_list.append(0)
+
+            elif ('asdiv' in args.test_data_file) or ('mawps' in args.test_data_file) or ('svamp' in args.test_data_file):
+                pred = pred_info['samples'][0]
+                label = label_info['completion']
+
+                pred_result = get_final_result_gsm8k(pred)
+                label_result = get_final_result_gsm8k(label)
+
+                pred_result = round(pred_result, 2)
+                label_result = round(label_result, 2)
+
+                if pred_result == label_result:
+                    acc_list.append(1)
+                else:
+                    acc_list.append(0)
+
+            elif 'csqa' in args.test_data_file:
+                pred = pred_info['samples'][0]
+                label = label_info['answer']
+                candidates = label_info['candidates']
+
+                assert label in candidates
+
+                score = get_csqa_match(pred, label, candidates)
+                acc_list.append(score)
+
             else:
                 pdb.set_trace()
-
-            assert label_result is not None
-            assert label_result != ''
-            assert label_result != 0
-
-            if pred_result == label_result:
-                acc_list.append(1)
-            else:
-                acc_list.append(0)
 
     print('acc:', sum(acc_list) / len(acc_list))
 
